477f87cad67b2922a7013a96a1aae7e016595ac2
Alex Garnett <axfelix@gmail.com>
2021-01-06 15:36:32 -0800

    extension works, tweaking for submission

---
+++ doesitbeep/background.js
@@ -0,0 +1,29 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlMatches: 'https://www.nytimes.com/wirecutter/reviews/*' },
+          }),
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_page_action"});
+  });
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "open_new_tab" ) {
+      chrome.tabs.create({"url": request.url});
+    }
+  }
+);
---
+++ doesitbeep/doesitbeep.js
@@ -0,0 +1,32 @@
+// content.js
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "clicked_page_action" ) {
+            var matched = false;
+            a = FuzzySet();
+            $.getJSON('https://doesitbeep.com/api/beep.json', function(data) {
+                var jsonld = document.querySelectorAll('script[type="application/ld+json"]');
+                for (var i = 0; i < jsonld.length; i++) {
+                    var jsondata = JSON.parse(jsonld[i].innerText);
+                    if (jsondata["@type"] === "ItemList") {
+                        var itemName = jsondata["itemListElement"][0]["item"]["name"];
+                        a.add(itemName);
+                        for (var product in data) {
+                            var match = a.get(decodeURIComponent(data[product]["title"]));
+                            console.log(match);
+                            if (match !== null) {
+                                chrome.runtime.sendMessage({"message": "open_new_tab", "url": "https://doesitbeep.com/".concat(data[product]["url"])});
+                                matched = true;
+                                break;
+                            }
+                        }
+                        if (matched === false) {
+                            var url = "https://github.com/axfelix/doesitbeep/issues/new?assignees=axfelix&labels=BEEPS&template=new-thing-that-beeps.md&title=".concat(encodeURIComponent(itemName)); 
+                            chrome.runtime.sendMessage({"message": "open_new_tab", "url": url});
+                        }
+                    }
+                }
+            });
+        }
+    }
+);
